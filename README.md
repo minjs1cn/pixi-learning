dbb02ce798c1819a91a4b87603def7399b1b27fd
minjs1cn <303890562@qq.com>
2021-03-24 10:06:27 +0800

    doc

--- README.md
+++ README.md
@@ -6,8 +6,29 @@
 - [pixijs游戏开发](https://segmentfault.com/blog/pixijs)
 - [中文文档](https://www.bookstack.cn/read/LearningPixi/README.md)
 - [官方文档](http://pixijs.download/release/docs/index.html)
+- [pixijs中文文档](https://aitrade.ga/pixi.js-cn/)
 - [官方仓库](https://github.com/pixijs/pixi.js)
+- [pixijs公开聊天室](https://gitter.im/pixijs/pixi.js)
+
+## 粒子效果
+
+- [dust](https://github.com/kittykatattack/dust)
 
 ## 物理引擎
 
 - [matter-js](https://blog.csdn.net/chenqiong1991)
+
+## 碰撞检测
+
+- [bump](https://github.com/kittykatattack/bump)
+
+## 补间动画
+
+- [tween.js](https://github.com/tweenjs/tween.js)
+- [dynamics.js](https://github.com/michaelvillar/dynamics.js)
+- [charm](https://github.com/kittykatattack/charm)
+
+## 交互工具
+
+- [简单交互](https://github.com/kittykatattack/tink)
+- [多点触控交互](http://hammerjs.github.io/getting-started/)
--- src/tiling-sprite/index.ts
+++ src/tiling-sprite/index.ts
@@ -1,4 +1,4 @@
-import { TilingSprite } from 'pixi.js'
+import { Graphics, Sprite, TilingSprite } from 'pixi.js'
 import { utils } from 'pixi.js'
 import { createApp } from '../app'
 import dnf from '../assets/brick.jpeg'
@@ -27,10 +27,7 @@ function init() {
 }
 
 function setup() {
-  const sprite = new TilingSprite(utils.TextureCache['dnf'], 200, 200)
-  sprite.x = 100
-  sprite.y = 100
-
+  // 两个平铺精灵，利用速度差打造3D效果
   const wood = new TilingSprite(utils.TextureCache['wood'], 1286, 640)
   const ground = new TilingSprite(utils.TextureCache['ground'], 1286, 179)
 
@@ -38,7 +35,32 @@ function setup() {
   ground.y = app.view.scrollHeight - 179
   app.stage.addChild(wood)
   app.stage.addChild(ground)
+
+  // 平铺精灵的纹理坐标实现循环
+  const sprite = new TilingSprite(utils.TextureCache['dnf'], 200, 200)
+  sprite.x = app.view.scrollWidth - 200
+  sprite.y = 0
   app.stage.addChild(sprite)
+
+  // 普通精灵可以着色
+  const sprite1 = new Sprite(utils.TextureCache['dnf'])
+  sprite1.tint = 0xFFFF660
+  app.stage.addChild(sprite1)
+
+  // 对精灵加蒙版
+  const sprite2 = new Sprite(utils.TextureCache['dnf'])
+  sprite2.tint = 0xFFFF660
+  sprite2.x = 100
+  sprite2.y = 100
+  const rectangle = new Graphics()
+  rectangle.beginFill(0x66CCFF)
+  rectangle.drawCircle(0, 0, 50)
+  rectangle.endFill()
+  rectangle.x = 50
+  rectangle.y = 50
+  sprite2.mask = rectangle
+  app.stage.addChild(sprite2)
+  sprite2.addChild(rectangle)
 
 
   app.ticker.add((dt) => {
